48934db74cc12e06ab30d6cf9b745331e9dc829a
Mae <mae6666666666>
2026-04-26 12:01:11 +0100

    I added a feature that lets multiple people use the program at one using UUID

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import requests
 from ddgs import DDGS
 from openai import OpenAI
 from ai_resources import *
+import uuid
 
 
 
@@ -16,54 +17,72 @@ app = FastAPI()
 ai_description = AI_DESCRIPTION
 client = client
 
+# card_list = []
+# reason_data = ""
+card_list = {}
+reason_data = {}
 
-card_list = []
-reason_data = ""
+
+@app.get("/get_id")
+def get_id():
+    myuuid = uuid.uuid4()
+    return myuuid
+
 
 
 class Reason(BaseModel):
+    uuid: str
     reason: str
 
 
 @app.post("/reason")
 def reason(reason: Reason):
     global reason_data
-    reason_data = reason.reason
+    global card_list
+    card_list[reason.uuid] = []
+    reason_data[reason.uuid] = reason.reason
     return{"reason": reason.reason}
 
 @app.get("/get_card")
-def get_card():
-    length_card = len(card_list)
-    card = random.choice(cards)
-    if length_card < 3:
-        card_list.append(card)
-    if reason_data == "":
+def get_card(uuid: str):
+    if uuid not in reason_data:
         return "No reason given, so I can't consult the stars..."
-    else:
-        return{"Heres your card": card_list}
+    
+    length_card = len(card_list[uuid])
+    card = random.choice(cards)
 
-# def ai_message():
-#     messages = [
-#             {"role": "system", "content": AI_DESCRIPTION["system_prompt"]},
-#             {"role": "user", "content": f"""
-#             Reason for reading: {reason}
-#             Cards drawn: {card_list}
-#     """}]
+    if length_card < 3:
+        card_list[uuid].append(card)
 
-#     response = client.chat.completions.create(
-#                 model=MODEL,
-#                 messages=messages,
-#             )
-
-#     message = response.choices[0].message.content
-#     return message
+    return{"Heres your card": card_list[uuid]}
 
 
 @app.get("/overview")
-def overview():
-    message = ai_message(card_list=card_list, reason=reason_data)
-    if len(card_list) <= 2:
+def overview(uuid: str):
+    message = ai_message(card_list=card_list[uuid], reason=reason_data[uuid])
+    if len(card_list[uuid]) <= 2:
         return "Not enough cards given. The stars can't work if not enough information is given!"
     else:
         return{"message": message}
+    
+
+
+# card_list = []
+
+# card_list.append("dfd")
+
+# card_list = {}
+
+# card_list["mydofkjhdsoi"] = []
+
+# card_list["mydofkjhdsoi"].append("dfd")
+
+
+# card_list = {
+#     "dfd":[],
+#     "user 2":[],
+#     "user 3":[],
+#     "user 4":[],
+#     "user 5":[],
+# }
 
